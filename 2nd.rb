e5fc7c0ecd25acd0b7f609ae66f4af0fdbaceb46
n2kkrd <65614635+n2kkrd@users.noreply.github.com>
2021-03-20 12:36:04 +0300

    Create 2nd.rb

---
+++ 2nd.rb
@@ -0,0 +1,9 @@
+=begin
+puts "Как Вас зовут?"
+name = gets.chomp
+puts "Привет," + name
+=end
+stroka = 'Одна большая строка с разными символами'
+kolvo = stroka.length
+puts kolvo 
+puts stroka.reverse + " | " + stroka.upcase + " | " + stroka.downcase + " | " + stroka.capitalize
